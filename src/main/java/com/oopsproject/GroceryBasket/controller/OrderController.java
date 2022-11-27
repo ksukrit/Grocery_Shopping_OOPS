5b131b30b765736437065b282d73a9a38abe1c2d
sukrit966 <ksukrit2001@gmail.com>
2022-11-27 16:05:52 +0530

    Buy now working maybe ?

--- src/main/java/com/oopsproject/GroceryBasket/controller/OrderController.java
+++ src/main/java/com/oopsproject/GroceryBasket/controller/OrderController.java
@@ -4,11 +4,13 @@ import com.oopsproject.GroceryBasket.model.*;
 import com.oopsproject.GroceryBasket.service.*;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -29,14 +31,48 @@ public class OrderController {
     @RequestMapping("/buyNow/{productId}")
     public String buyNow(@PathVariable("productId") String productId){
         CustomerOrder customerOrder = new CustomerOrder();
+
         Cart c = new Cart();
         CartItem ci = new CartItem();
         ci.setCart(c);
-//        ci.setProduct();
-//        c.setCartItem();
-        // Make new cart with just single product
-        // TODO: Work on this
-        return "WIP";
+        Product p = productService.getProductById(productId);
+        ci.setProduct(p);
+        ci.setQuality(1);
+        ci.setPrice(p.getProductPrice());
+        c.setTotalPrice(p.getProductPrice());
+        c.setCartItem(Collections.singletonList(ci));
+
+        if(p.getUnitStock() < 1){
+            return "Insufficient Stock";
+        }
+        p.setUnitStock(p.getUnitStock()-1);
+
+        customerOrder.setOrderDate(System.currentTimeMillis());
+        customerOrder.setDeliveryDate(System.currentTimeMillis()+p.getDeliveryDate()*1000L);
+
+        Customer customer;
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(user == null) return "";
+        customer = customerService.getCustomerByemailId(user.getUsername());
+
+        if(c.getTotalPrice() > customer.getWalletBalance()){
+            return "Insufficient Balance";
+        }
+
+        c.setCustomer(customer);
+        customerOrder.setCart(c);
+        customerOrder.setCustomer(customer);
+        customerOrder.setShippingAddress(customer.getShippingAddress());
+
+        customerOrderService.addCustomerOrder(customerOrder);
+
+        productService.editProduct(p);
+
+        customer.setWalletBalance(customer.getWalletBalance() - c.getTotalPrice());
+
+        customerService.updateCustomer(customer);
+
+        return "Ordered Successfully " + customerOrder.getCustomerOrderId();
     }
 
     private double getCartTotal(Cart cart){
